f6c3679136daf13961bd8042f3c12c78e9c9f1b5
Labalve <labalve@gmail.com>
2018-01-29 19:59:58 +0100

    :sparkles: getting started!

---
+++ main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+char * inputHandler;
+char inputParamHandler[10][10];
+
+void welcomeScreen(){
+    printf("\n\t==========C-Shell==========\n");
+    printf("\t==author: Krzysztof Kulak==\n");
+    printf("\t===========================\n\n");
+}
+
+void exitBash(int status){
+    exit(status);
+}
+
+int printPrompt()
+{
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        fprintf(stdout, "[%s] ", cwd);
+        return 0;
+    }
+    else {
+        printf("error occured while setting working directory");
+        return 1;
+    }
+}
+
+void getInput()
+{
+    char input[100];
+    scanf ("%[^\n]%*c", input);
+    const char s[2] = " ";
+    char *token;
+
+    token = strtok(input, s);
+    int i = 0;
+    while( token != NULL ) {
+        if(i == 0) inputHandler = token;
+        else strcpy(inputParamHandler[(i-1)], token);
+        printf( " %s\n", token );
+    //inputHandlingArray[i++] = token;
+        token = strtok(NULL, s);
+        i++;
+   }
+}
+
+void actionManager()
+{
+    //printf("%s", *inputHandler);
+    if(*inputHandler == "exit"){
+        exit(*inputParamHandler[0]);
+        printf("exituje sb :)");
+    }
+}
+
+int main()
+{
+    welcomeScreen();
+    while(1){
+        if(printPrompt() == 1){
+            return 1;
+        };
+        getInput();
+    //printf("%s", inputHandler);
+
+        actionManager();
+    }
+
+/*    printf("%s", inputParamHandler[0]);
+    printf("%s", inputParamHandler[1]);
+    printf("%s", inputParamHandler[2]);*/
+    return 0;
+}
